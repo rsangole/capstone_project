4d2cfa6c425280fefa8503484ee9711510c14be3
Rahul Sangole <rsangole@u.northwestern.edu>
2019-02-02 20:08:08 -0500

    Starting raw kaggle modeling in notebook

--- src/01-RawKaggle_Modeling.R
+++ src/01-RawKaggle_Modeling.R
@@ -1,51 +1,30 @@
----
-title: "Rahul's EDA"
-output: 
-  html_notebook: 
-    fig_height: 6
-    fig_width: 10
-    highlight: tango
-    theme: journal
----
-
-```{r message=FALSE, warning=FALSE, include=FALSE, paged.print=FALSE}
+## ----message=FALSE, warning=FALSE, include=FALSE, paged.print=FALSE------
 library("tidyverse")
 library("xts")
 library("dygraphs")
 library("janitor")
-```
 
-```{r}
+## ------------------------------------------------------------------------
 kag_train <- read_csv("../data/raw/kaggle/train.csv") %>% 
     clean_names()
 head(kag_train)
-```
 
-Data collected late-May --> early-Oct
-Every week:
-Mon-->Wed : Get Mosq
-By Sun : Test
-
-```{r}
+## ------------------------------------------------------------------------
 mosq_by_date = kag_train %>% group_by(date,trap) %>% summarise(total_mosq=sum(num_mosquitos),total_wnv = sum(wnv_present)) %>% ungroup()
 
 trap_metadata = kag_train %>% select(trap, latitude, longitude, address_accuracy) %>% distinct()
-```
 
-```{r}
+## ------------------------------------------------------------------------
 head(mosq_by_date)
-```
 
-```{r}
+## ------------------------------------------------------------------------
 head(trap_metadata)
-```
 
-```{r}
+## ------------------------------------------------------------------------
 ohare = c(41.99,-87.933,662)
 midway = c(41.786,-87.752,612)
-```
 
-```{r}
+## ------------------------------------------------------------------------
 hav.dist <- function(long1, lat1, long2, lat2) {
   R <- 6371
   diff.long <- (long2 - long1)
@@ -55,40 +34,30 @@ hav.dist <- function(long1, lat1, long2, lat2) {
   d = R * b
   return(d)
 }
-```
 
-```{r}
+## ------------------------------------------------------------------------
 trap_metadata %>% 
     mutate(ohare_dist = hav.dist(longitude,ohare[2],latitude,ohare[1]),
            midway_dist = hav.dist(longitude,midway[2],latitude,midway[1])) -> trap_metadata
-```
 
-
-```{r}
+## ------------------------------------------------------------------------
 trap_counts = mosq_by_date %>% count(trap)
 trap_metadata %>% 
     left_join(trap_counts) %>% 
     ggplot(aes(longitude,latitude,color=n,size=n),alpha=0.4)+
     geom_point()+
     coord_fixed(ratio = 1)
-```
 
-## Weather Data
-
-```{r}
+## ------------------------------------------------------------------------
 weather_data <- read_csv("../data/raw/kaggle/weather.csv")
 head(weather_data)
-```
 
-Pre-processing weather data...
-
-```{r}
+## ------------------------------------------------------------------------
 date_vec = weather_data$Date
 int_cols = weather_data %>% select_if(is_numeric) %>% select(-Date)
 chr_cols = weather_data %>% select_if(is_character)
-```
 
-```{r}
+## ------------------------------------------------------------------------
 replace_m_with_na <- function(x){
     x[x=="M"] = NA
     x[x=="-"] = NA
@@ -98,21 +67,16 @@ replace_t_with_na <- function(x){
     x[x=="T"] = NA
     x
 }
-```
 
-```{r}
+## ------------------------------------------------------------------------
 int_cols = int_cols %>% purrr::map_df(~replace_m_with_na(.x)) %>% purrr::map_df(~replace_t_with_na(.x))
 chr_cols = chr_cols %>% purrr::map_df(~replace_m_with_na(.x)) %>% purrr::map_df(~replace_t_with_na(.x))
-```
 
-```{r}
+## ------------------------------------------------------------------------
 weather_data = tibble(date = date_vec) %>% bind_cols(int_cols, chr_cols)
 head(weather_data)
-```
 
-More features:
-
-```{r}
+## ------------------------------------------------------------------------
 weather_data <- weather_data %>% 
     mutate(
         Sunset = lubridate::fast_strptime(as.character(Sunset),format = "%H%M",lt = FALSE),
@@ -120,37 +84,30 @@ weather_data <- weather_data %>%
         day_time = as.numeric(Sunset-Sunrise)
     ) %>% 
     select(-Sunset,-Sunrise)
-```
 
-```{r}
+## ------------------------------------------------------------------------
 weather_data <- weather_data %>% select(-CodeSum)
-```
 
-```{r}
+## ------------------------------------------------------------------------
 weather_data[,-1] <- weather_data[,-1] %>% purrr::map_df(~as.numeric(.x))
 head(weather_data)
-```
 
-Impute NA
-
-```{r}
+## ------------------------------------------------------------------------
 library(missForest)
 library(doParallel)
 library(foreach)
 #cl <- parallel::makeCluster(18)
 registerDoParallel(cores = 15)
 mfFit <- missForest(xmis = as.matrix(weather_data[,-1]),ntree = 150)
-```
-```{r}
+
+## ------------------------------------------------------------------------
 weather_impute = mfFit$ximp
 weather_impute = tibble(date = weather_data$date) %>% bind_cols(as_tibble(weather_impute))
-```
 
-```{r}
+## ------------------------------------------------------------------------
 head(weather_impute)
-```
 
-```{r}
+## ------------------------------------------------------------------------
 train_X = mosq_by_date %>% 
     left_join(trap_metadata) %>% 
     left_join(weather_impute) %>% 
@@ -159,20 +116,15 @@ train_X = mosq_by_date %>%
     mutate(SnowFall = round(SnowFall,2)) %>% 
     mutate_at(c("year","month","date"),as.numeric) %>% 
     mutate(trap=as.factor(trap))
-```
 
-```{r}
+## ------------------------------------------------------------------------
 head(train_X)
-```
 
-
-```{r}
+## ------------------------------------------------------------------------
 dim(train_X)
-```
 
-```{r}
+## ------------------------------------------------------------------------
 library(randomForest)
 ?randomForest
 randomForest(x=train_X[,c(-4,-6)],y = train_X[[6]],ntree = 150)
-```
 
